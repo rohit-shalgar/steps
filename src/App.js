f3e1da58600924b18b36f796d3d5c207ba4cb787
Capgemini <capgemini@Capgeminis-MacBook-Pro.local>
2024-05-19 16:28:16 -0600

    steps first version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
 
 function App() {
+  const [currentStep, setStep] = useState(1)
+
+  function handlerPrevious(){
+    if(currentStep > 1)
+      setStep(currentStep-1)
+  }
+
+  function handleNext(){
+    if(currentStep < 3)
+      setStep(currentStep+1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="steps">
+      <div className="numbers">
+        <div className={currentStep >=1 ? "active": ""}>1</div>
+        <div className={currentStep >=2 ? "active": ""}>2</div>
+        <div className={currentStep >=3 ? "active": ""}>3</div>
+      </div>
+      <div className="message">Step {currentStep}: {messages[currentStep -1]}</div>
+      <div className="buttons">
+        <button style={{backgroundColor:'purple',color:'white'}} onClick={handlerPrevious}>previous</button>
+        <button style={{backgroundColor:'purple',color:'white'}} onClick={handleNext}>next</button>
+      </div>
     </div>
   );
 }
